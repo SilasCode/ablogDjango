20e56db5df76538ae405cd050fb55c3ce988e4ed
Silas Code <silasmiley25@gmail.com>
2023-10-13 22:38:10 +0200

    tweaked app for heroku

--- ablog/settings.py
+++ ablog/settings.py
@@ -84,12 +84,16 @@ WSGI_APPLICATION = 'ablog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
