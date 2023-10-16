8e81e562fb26980ebb975f648104c5154bb990a2
Silas Code <silasmiley25@gmail.com>
2023-10-16 05:52:33 +0200

    Added sqlite3

--- ablog/settings.py
+++ ablog/settings.py
@@ -90,7 +90,11 @@ WSGI_APPLICATION = 'ablog.wsgi.application'
 
 # Parse the DATABASE_URL environment variable provided by Heroku
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
@@ -135,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
