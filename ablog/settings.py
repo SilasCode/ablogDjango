b0181fc9b921d34b2d03ef4d9908a004dba0107e
Silas Code <silasmiley25@gmail.com>
2023-10-16 13:45:43 +0200

    tweaked app for heroku

--- ablog/settings.py
+++ ablog/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 import django_heroku
-import psycopg2
 import dj_database_url
 from decouple import config
 
@@ -31,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*tlq#x*j*2(53p%vpn3xhx)48h%&ch=g+$ll)=z&glbdn3n+(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simblog-ceea4a316708.herokuapp.com']
 
 
 # Application definition
@@ -84,15 +83,14 @@ WSGI_APPLICATION = 'ablog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-# Configure the database using dj-database-url
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-# Ensure that SSL is required for the connection
-DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
