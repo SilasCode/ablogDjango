ccb57319fd48aa45fdb505045c8989708d61f03e
Silas Code <silasmiley25@gmail.com>
2023-10-18 21:57:09 +0200

    test postsgresql

--- ablog/settings.py
+++ ablog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-*tlq#x*j*2(53p%vpn3xhx)48h%&ch=g+$ll)=z&glbdn3n+(q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simblog-ceea4a316708.herokuapp.com']
 
 
 # Application definition
@@ -84,12 +84,7 @@ WSGI_APPLICATION = 'ablog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
