bf5e935e1dcba8476c811781ec8992713bc39925
Silas Code <silasmiley25@gmail.com>
2023-11-19 14:45:45 +0200

    added trans app for heroku

--- theblog/forms.py
+++ theblog/forms.py
@@ -11,7 +11,7 @@ for item in choices:
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
-		fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image', 'video_file', 'photo_credit', 'post_highlighted')
+		fields = ('title', 'title_tag', 'author', 'category', 'french_body', 'body', 'snippet', 'header_image', 'video_file', 'photo_credit', 'post_highlighted')
 
 		widgets = {
 			'title': forms.TextInput(attrs={'class': 'form-control'}),
@@ -20,19 +20,21 @@ class PostForm(forms.ModelForm):
 			#'author': forms.Select(attrs={'class': 'form-control'}),
 			'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
 			'body': forms.Textarea(attrs={'class': 'form-control'}),
+			'french_body': forms.Textarea(attrs={'class': 'form-control'}),
 			'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': ' Write a short paragraph for our post'}),
 		}
 
 class EditForm(forms.ModelForm):
 	class Meta:
 		model = Post
-		fields = ('title', 'title_tag', 'body', 'snippet', 'header_image', 'video_file', 'photo_credit')
+		fields = ('title', 'title_tag', 'french_body', 'body', 'snippet', 'header_image', 'video_file', 'photo_credit')
 
 		widgets = {
 			'title': forms.TextInput(attrs={'class': 'form-control'}),
 			'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
 			#'author': forms.Select(attrs={'class': 'form-control'}),
 			'body': forms.Textarea(attrs={'class': 'form-control'}),
+			'french_body': forms.Textarea(attrs={'class': 'form-control'}),
 			'snippet': forms.Textarea(attrs={'class': 'form-control'}),
 		}
 
--- theblog/models.py
+++ theblog/models.py
@@ -41,6 +41,7 @@ class Post(models.Model):
 	title_tag = models.CharField(max_length=255)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	body = RichTextField(blank=True, null=True)
+	french_body = RichTextField(blank=True, null=True)
 	#body = models.TextField()
 	post_date = models.DateField(auto_now_add=True)
 	category = models.CharField(max_length=255, default='')
